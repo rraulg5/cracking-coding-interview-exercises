cdf66e18f2c395c566d896fef4427894e79f26aa
Raul Garcia <rraulg5@gmail.com>
2023-01-19 19:52:04 -0600

    implement your own version of StringBuilder

--- src/App.java
+++ src/App.java
@@ -1,5 +1,22 @@
+import ArraysAndStrings.MyStringBuilder;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        MyStringBuilder sb = new MyStringBuilder("Hello ");
+        String world = "World";
+        String exclamationMark = "!";
+
+        /* StringBuilder before appending */
+        System.out.println("Builder: " + sb);
+
+        /* Appending */
+        sb.append(world);
+        sb.append(exclamationMark);
+
+        /* StringBuilder before appending */
+        System.out.println("After append: " + sb);
+
+        sb.reverse();
+        System.out.println("After reversed: " + sb);
     }
 }
---
+++ src/ArraysAndStrings/MyStringBuilder.java
@@ -0,0 +1,49 @@
+package ArraysAndStrings;
+
+public class MyStringBuilder {
+    private int count = 0;
+    private char[] value;
+
+    public MyStringBuilder() {
+        value = new char[16];
+    }
+
+    public MyStringBuilder(String str) {
+        value = new char[str.length()];
+        append(str);
+    }
+
+    public void append(char c) {
+        ensureCapacityInternal();
+        value[count++] = c;
+    }
+
+    public void append(String s) {
+        for (int i = 0; i < s.length(); i++) {
+            append(s.charAt(i));
+        }
+    }
+
+    public void reverse() {
+        char[] reversed = new char[count];
+        for (int i = 0; i < count; i++) {
+            reversed[i] = value[count - 1 - i];
+        }
+        value = reversed;
+    }
+
+    private void ensureCapacityInternal() {
+        if (count + 1 == value.length) {
+            char[] newValue = new char[value.length * 2];
+            for (int i = 0; i < value.length; i++) {
+                newValue[i] = value[i];
+            }
+            value = newValue;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return new String(value);
+    }
+}
