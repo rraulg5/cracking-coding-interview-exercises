b7b0328a5d26a7486fcaa7f4e1a348445c4b4408
Raul Garcia <rraulg5@gmail.com>
2023-01-21 19:22:24 -0600

    chapter 1 | question 1

---
+++ src/Ch01_ArraysAndStrings/Q01_Is_Unique/QuestionA.java
@@ -0,0 +1,26 @@
+package Ch01_ArraysAndStrings.Q01_Is_Unique;
+
+import java.util.Hashtable;
+
+public class QuestionA {
+
+    public static boolean isUnique(String str) {
+        Hashtable<Character, Integer> hashTable = new Hashtable<Character, Integer>();
+
+        for (int i = 0; i < str.length(); i++) {
+            if (hashTable.containsKey(str.charAt(i))) {
+                return false;
+            }
+            hashTable.put(str.charAt(i), 1);
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String[] words = { "abc", "aba", "programmer", "cat", "wolverine", "black", "hello", "world" };
+        for (String s : words) {
+            System.out.println(s + ": " + isUnique(s));
+        }
+    }
+}
