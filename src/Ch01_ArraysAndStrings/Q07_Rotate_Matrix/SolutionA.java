34e779aa4f4a6dc3ace7b121d6aa09be7d30d6c8
Raul Garcia <rraulg5@gmail.com>
2023-01-26 19:04:08 -0600

    chapter 1 | question 7 | book solution

---
+++ src/Ch01_ArraysAndStrings/Q07_Rotate_Matrix/SolutionA.java
@@ -0,0 +1,42 @@
+package Ch01_ArraysAndStrings.Q07_Rotate_Matrix;
+
+public class SolutionA {
+
+    // book solution
+    public static boolean rotate(int[][] matrix) {
+
+        if (matrix.length == 0 || matrix.length != matrix[0].length)
+            return false;
+
+        int n = matrix.length;
+        for (int layer = 0; layer < n / 2; layer++) {
+            int first = layer;
+            int last = n - 1 - layer;
+
+            for (int i = first; i < last; i++) {
+                int offset = i - first;
+
+                int top = matrix[first][i];
+
+                // left -> top
+                matrix[first][i] = matrix[last - offset][first];
+
+                // bottom -> left
+                matrix[last - offset][first] = matrix[last][last - offset];
+
+                // right -> bottom
+                matrix[last][last - offset] = matrix[i][last];
+
+                // top -> right
+                matrix[i][last] = top;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        rotate(new int[8][8]);
+    }
+}
